ab51c76e7a53a4c334443fc9548550167fafa32f
agpiermarini <piermarini.andrew@gmail.com>
2018-05-10 11:23:51 -0600

    Add github api following endpoint

---
+++ app/services/following_search.rb
@@ -0,0 +1,23 @@
+class FollowingSearch
+  def initialize(username, token)
+    @username = username
+    @token = token
+  end
+
+  def following
+    connection = Faraday.new "https://api.github.com/users/#{username}/following"
+
+    response = connection.get do | req |
+      req.headers["Authorization"] = "token #{token}"
+    end
+
+    follower_info  = JSON.parse(response.body, symbolize_names: true)
+
+    follower_info.map do | follower |
+      UserSearch.new(follower[:login], ENV["github_token"]).profile
+    end
+  end
+
+  private
+  attr_reader :username, :token
+end
